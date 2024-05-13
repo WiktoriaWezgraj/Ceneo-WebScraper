1df4ac0de2589481f2b7c8b50b3e6474369454b1
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2024-05-13 18:17:02 +0200

    2024-05-13 WW

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
+@app.route('/extract', methods = ['POST', 'GET'])
 def extract():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        url= f"https://www.ceneo.pl/{product_id}"
+        response = requests.get(url)
+        if response.status_code == requests.codes['ok']:
+            pass
+
+        return redirect(url_for('product', product_id =product_id))
     return render_template("extract.html")
 
 @app.route('/products')
